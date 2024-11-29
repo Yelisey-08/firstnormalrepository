5b86389af3b842f986586187c66db0eb0a10bfb7
admin <polyanskiy.yelisey@mail.ru>
2024-11-29 21:48:38 +0500

    My 6 commit

--- niam.py
+++ niam.py
@@ -1,7 +1,9 @@
-print(3)
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('My first git program')
+    print('And I change it every day')
 
-print(2)
 
-print(2)
-
-print(3)
+if __name__ == '__main__':
+    main()
