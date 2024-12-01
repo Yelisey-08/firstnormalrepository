833639b85e5fe3be336c1a7cc33314bb4f566dc8
Yelisey-08 <elisey.polyanski@gmail.com>
2024-12-01 09:47:24 +0500

    Update niam.py

--- niam.py
+++ niam.py
@@ -1,8 +1,10 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('And I change it every day')
 
 
 if __name__ == '__main__':
     main()
+
+n = input()
+print(n)
